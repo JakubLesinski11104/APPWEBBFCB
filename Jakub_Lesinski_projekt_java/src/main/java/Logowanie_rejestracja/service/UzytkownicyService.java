2a473e0daa914c29930bbadcc8fa1c9ac75c6c3e
JakubLesinski11104 <104146699+JakubLesinski11104@users.noreply.github.com>
2022-06-22 14:12:04 +0200

    Delete Jakub_Lesinski_projekt_java directory

--- Jakub_Lesinski_projekt_java/src/main/java/Logowanie_rejestracja/service/UzytkownicyService.java
+++
@@ -1,7 +0,0 @@
-package Logowanie_rejestracja.service;
-import Logowanie_rejestracja.model.Uzytkownicy;
-public interface UzytkownicyService {
- public void registerUzytkownicy(Uzytkownicy uzytkownicy);
- 
- public Uzytkownicy loginUzytkownicy(Uzytkownicy uzytkownicy);
-}
